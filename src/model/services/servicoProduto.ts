479aea833800e7b48f77d6d87a7f404d3485cecc
MailsonSousa88 <legionslmax86@gmail.com>
2026-09-19 07:34:05 -0300

    fix(model): corrige a lógica da busca de produtos por categoria

--- src/model/services/servicoProduto.ts
+++ src/model/services/servicoProduto.ts
@@ -10,14 +10,14 @@ export class servicoProduto {
 
   /**
    * Objetivo: Utilizar data source de produto para encontrar todos os produtos com a mesma categoria fornecida.
-   * @param categoria A categoria que é fornecida pelo usuário para fazer a pesquisa.
+   * @param categoriaId O id categoria que é fornecida pelas ações do usuário na View para fazer a pesquisa.
    * @returns Uma lista de produtos de uma mesma categoria.
    */
   public async pesquisarProdutoPorCategoria(
-    categoria: string,
+    categoriaId: string,
   ): Promise<Array<Produto> | undefined> {
     const PRODUTOS_CATEGORIA: Array<Produto> =
-      await this.produtoDataSource.buscarProdutoPorCategoria(categoria);
+      await this.produtoDataSource.buscarProdutoPorCategoria(categoriaId);
 
     if (PRODUTOS_CATEGORIA) {
       throw new Error("Não foi possivel localizar produtos dessa categoria.");
